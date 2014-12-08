6873953d1beda5dd95952a3ae60524a7c3662674
eronth <eronth@gmail.com>
2014-12-07 23:54:02 -0600

    Created a constants class to handle all the possible actions a creature can have.

---
+++ SurvivalTrials/src/com/mtank/constants/Action.java
@@ -0,0 +1,14 @@
+package com.mtank.constants;
+
+public class Action {
+	public static final int IDLE=0;
+	public static final int WANDER=1;
+	public static final int WALK=2;
+	public static final int GRAB=3;
+	public static final int GATHER=4;
+	public static final int DESTROY=5; 
+	public static final int BUILD=6;
+	public static final int SLEEP=7;
+	//public static final int =8;
+	
+}
